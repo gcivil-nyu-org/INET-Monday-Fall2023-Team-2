9445aeea4d54fdbc178fb31f87e4be5448edbb86
AshleyAslihanGoluoglu <119933910+AshleyAslihanGoluoglu@users.noreply.github.com>
2023-10-13 15:36:37 +0300

    only display posted posts, change poster from id to user object

--- symbi/posts/models.py
+++ symbi/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 
@@ -11,7 +12,7 @@ class ActivityTag(models.Model):
 
 
 class ActivityPost(models.Model):
-    poster_id = models.CharField(max_length=20)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -35,7 +36,7 @@ class Comment(models.Model):
     post = models.ForeignKey(
         ActivityPost, on_delete=models.CASCADE, related_name="comments"
     )
-    poster_id = models.CharField(max_length=20)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     timestamp = models.DateTimeField("date commented")
 
