29f9583fea222e7ffdd323de288290448390177c
AshleyAslihanGoluoglu <119933910+AshleyAslihanGoluoglu@users.noreply.github.com>
2023-10-13 12:30:24 +0300

    added home view

--- symbi/posts/views.py
+++ symbi/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import ActivityPost, ActivityTag, Comment
+
+class HomeView(generic.ListView):
+    template_name = 'posts/home.html'
+    context_object_name = 'latest_posts_list'
+
+    def get_queryset(self):
+        """Return the last five published posts."""
+        return ActivityPost.objects.order_by('-timestamp')[:5]
