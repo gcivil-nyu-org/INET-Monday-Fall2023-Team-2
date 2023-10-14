1c746c991dc6148bbc8bfbcedbcabe2237373633
louispo0603 <118866618+louispo0603@users.noreply.github.com>
2023-10-14 18:51:45 -0400

    #45
    added archivePost in views
    added button for archive in post_detail template
    added url for archivePost

--- symbi/posts/urls.py
+++ symbi/posts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create_post/', views.CreatePostView.as_view(), name='create_post'),
     path('createPost/', views.createPost, name='createPost'),
     path('<int:post_id>/delete/', views.deletePost, name='deletePost'),
+    path('<int:post_id>/archive/', views.archivePost, name='archivePost'),
 ]
