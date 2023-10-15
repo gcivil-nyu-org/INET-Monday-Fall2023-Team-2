4b1ad2305ccc46d2561aa34fcb917a7ea4e83d2b
Tarik Kassa <tk2766@nyu.edu>
2023-10-15 06:41:49 -0400

    edit post implementation

--- symbi/posts/urls.py
+++ symbi/posts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('create_post/', views.CreatePostView.as_view(), name='create_post'),
     path('createPost/', views.createPost, name='createPost'),
     path('<int:post_id>/delete/', views.deletePost, name='deletePost'),
+    path('edit/<int:pk>/', views.EditPostView.as_view(), name='edit_post'),
+    path('edit/', views.edit_post, name='editPost'),
 ]
--- symbi/posts/views.py
+++ symbi/posts/views.py
@@ -43,3 +43,21 @@ def createPost(request):
 def deletePost(request, post_id):
     ActivityPost.objects.filter(pk=post_id).delete()
     return HttpResponseRedirect(reverse('posts:home'))
+
+class EditPostView(generic.UpdateView):
+    model = ActivityPost
+    template_name = 'posts/edit_post.html'
+    context_object_name = 'post'
+    fields = ['title', 'description']
+
+
+def edit_post(request):
+
+    title = request.POST.get('title')
+    description = request.POST.get('description')
+    post_id = request.POST.get('postId')
+    post = ActivityPost.objects.get(pk=post_id)
+    post.title = title
+    post.description = description
+    post.save()
+    return HttpResponseRedirect(reverse('posts:home'))
