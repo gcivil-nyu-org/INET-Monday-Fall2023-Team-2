72cc4c6ec7bff3f2b614ef2c832b7906c8337d57
MJ3128 <mj3128@nyu.edu>
2023-10-22 11:02:13 -0400

    added profile creation and profile details view

--- symbi/socialuser/views.py
+++ symbi/socialuser/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.shortcuts import render
 
-from .models import SocialUser
+from .models import InterestTag, SocialUser
+from .forms import SocialUserForm
 
-class ProfileView(generic.DetailView):
+
+class CreateProfileView(generic.CreateView):
     model = SocialUser
-    template_name = "posts/profile.html"
-    context_object_name = "socialuser"
+    form_class = SocialUserForm
+    template_name = "socialuser/create_profile.html"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "socialuser:profile_view",
+            kwargs={
+                "pk": self.user_id,
+            },
+        )
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+
+        self.user_id = self.object.pk
+
+        return super(CreateProfileView, self).form_valid(form)
+
+
+class ProfileDetailsView(generic.DetailView):
+    model = SocialUser
+    template_name = "socialuser/profile_details.html"
+    context_object_name = "profile"
+
+
+# def create_profile(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         age = request.POST.get("age")
+#         major = request.POST.get("major")
+#         pronouns = request.POST.get("pronouns")
+#         tags = request.POST.get("tags")
+
+#         new_profile = SocialUser.objects.create(
+#             name=name, age=age, major=major, pronouns=pronouns, tags=tags
+#         )
+
+#     interest_tags = InterestTag.objects.all()
+
+#     return render(request, "create_profile.html", {"interest_tags": interest_tags})
