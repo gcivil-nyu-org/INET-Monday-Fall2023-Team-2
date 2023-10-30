c7508fae321fb39c93e878ea090da0dd3483c0ef
louispo0603 <118866618+louispo0603@users.noreply.github.com>
2023-10-29 21:15:51 -0400

    Added Testcase for home view
    added test following the tutorial but it seems not working.

--- symbi/posts/tests.py
+++ symbi/posts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import ActivityPost
+import datetime
+
 
 # Create your tests here.
+def create_post(title, description, status):
+    # Status values:
+    # 1: Draft
+    # 2: Posted
+    # 3: Archived
+    time = timezone.now() + datetime.timedelta(days=0)
+    return ActivityPost.objects.create(
+        timestamp=time,
+        title=title,
+        description=description,
+        status=status,
+        tags="None")
+
+
+class ArchivePostTests(TestCase):
+    def view_post_test(self):
+        """
+        Home page should show an empty post list if there is only 1 archive post
+        """
+        post = create_post("post", "description", 3)
+        response = self.client.get("")
+        self.assertQuerysetEqual(response.context["latest_posts_list"], [])
+        self.assertContains(response, "No posts are available.")
+
+
