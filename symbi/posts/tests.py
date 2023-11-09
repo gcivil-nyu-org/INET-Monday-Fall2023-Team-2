b98492e18ea138f0354abe855e18b35558862901
Tarik Kassa <tk2766@nyu.edu>
2023-11-09 16:54:51 -0500

    updated tests.py

--- symbi/posts/tests.py
+++ symbi/posts/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
+
 from .models import ActivityPost
 from main.models import SocialUser
 import datetime
@@ -29,33 +30,64 @@ class TestHomePage(TestCase):
 class TestEditPost(TestCase):
     def setUp(self):
         # Create a test user
-        self.user = SocialUser.objects.create(
-            first_name="Test User", pronouns="she/her"
+        self.user = SocialUser.objects.create_user(
+            username="testuser", password="testpassword"
         )
 
-        # Create a test post associated with the current user
+        # Create a test post
         self.post = ActivityPost.objects.create(
-            title="Test Title1", description="Test Description1", poster=self.user
+            title="Test Post",
+            description="Test Description",
+            poster=self.user,
+            status=ActivityPost.Status.DRAFT,
         )
 
-    def test_edit_post_saves(self):
+        # Login the test user
+        self.client = Client()
+        self.client.login(username="testuser", password="testpassword")
+
+    def test_edit_post_save_changes(self):
+        # Simulate a POST request to save changes
         response = self.client.post(
             reverse("posts:edit_post_request", args=[self.post.id]),
-            data={
-                "title": "New Title",
-                "description": "New Description",
+            {
+                "title": "Updated Title",
+                "description": "Updated Description",
                 "action": "save",
             },
         )
-        # Check if the response redirects to the home page
+
+        # Check that the response is a redirect to the home page
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("main:home"))
 
-        # Check if the post has been updated
+        # Refresh the post from the database
         updated_post = ActivityPost.objects.get(pk=self.post.id)
-        self.assertEqual(updated_post.title, "New Title")
-        self.assertEqual(updated_post.description, "New Description")
-        if updated_post:
-            print("Post updated successfully!")
-        else:
-            print("Post editing failed.")
+
+        # Check that the post has been updated as a draft
+        self.assertEqual(updated_post.title, "Updated Title")
+        self.assertEqual(updated_post.description, "Updated Description")
+        self.assertEqual(updated_post.status, ActivityPost.Status.DRAFT)
+
+    def test_edit_post_post(self):
+        # Simulate a POST request to post the edited post
+        response = self.client.post(
+            reverse("posts:edit_post_request", args=[self.post.id]),
+            {
+                "title": "Updated Title",
+                "description": "Updated Description",
+                "action": "post",
+            },
+        )
+
+        # Check that the response is a redirect to the home page
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("main:home"))
+
+        # Refresh the post from the database
+        updated_post = ActivityPost.objects.get(pk=self.post.id)
+
+        # Check that the post has been updated and is now active
+        self.assertEqual(updated_post.title, "Updated Title")
+        self.assertEqual(updated_post.description, "Updated Description")
+        self.assertEqual(updated_post.status, ActivityPost.Status.ACTIVE)
