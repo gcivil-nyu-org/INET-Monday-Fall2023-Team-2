593bcd58dc7b49d4636c58667b97afa056204275
louispo0603 <118866618+louispo0603@users.noreply.github.com>
2023-11-11 15:46:24 -0500

    added test for posting and deleting comments
    added test cases

--- symbi/posts/tests.py
+++ symbi/posts/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.utils import timezone
-from .models import ActivityPost
+from .models import ActivityPost, Comment
 from main.models import SocialUser
 import datetime
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 # Create your tests here.
@@ -59,3 +60,64 @@ class TestEditPost(TestCase):
             print("Post updated successfully!")
         else:
             print("Post editing failed.")
+
+
+class CommentsTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser", password="TestPassword!23"
+        )
+        self.post = ActivityPost.objects.create(
+            title="Test Post", description="Test Description", poster=self.user
+        )
+
+    def test_comment_creation(self):
+        # Log in the user
+        self.client.login(username="testuser", password="TestPassword!23")
+
+        # Comment data
+        comment_text = "This is a test comment"
+
+        # Post a comment
+        response = self.client.post(
+            reverse("posts:add_comment", args=[self.post.id]), {"comment": comment_text}
+        )
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the comment was successfully posted
+
+        # Check if the comment is in the database
+        comment = Comment.objects.get(
+            post=self.post,
+            commentPoster=self.user,
+            text=comment_text,
+            timestamp=timezone.now(),
+        )
+        self.assertIsNotNone(comment)
+
+    def test_comment_deletion(self):
+        # Create a comment
+        comment = Comment.objects.create(
+            post=self.post,
+            commentPoster=self.user,
+            text="Test Comment",
+            timestamp=timezone.now(),
+        )
+
+        print("Starting test_comment_deletion")
+        print("Comment ID:", comment.id)
+
+        # Log in the user
+        self.client.login(username="testuser", password="TestPassword!23")
+
+        # Delete a comment
+        response = self.client.post(
+            reverse("posts:delete_comment", args=[self.post.id, comment.id])
+        )
+        self.assertEqual(
+            response.status_code, 302
+        )  # Check if the comment was successfully deleted
+
+        # Check if the comment is no longer in the database
+        with self.assertRaises(Comment.DoesNotExist):
+            Comment.objects.get(pk=comment.id)
