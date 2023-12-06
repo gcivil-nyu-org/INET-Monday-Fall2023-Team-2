fd680ef076819c8bcb25f75f9e1526dfe87e05a6
Utsav Oza <utsavoza@users.noreply.github.com>
2023-12-06 17:23:38 -0500

    Run migrations (#250)

--- symbi/chat/urls.py
+++ symbi/chat/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "chat"
 urlpatterns = [
     path("", views.ChatRoomListView.as_view(), name="chat_room_list"),
-    path("/<int:pk>/", views.ChatRoomView.as_view(), name="chat_room"),
+    path("<int:pk>", views.ChatRoomView.as_view(), name="chat_room"),
     path(
-        "/new/<slug:requester>/<slug:receiver>/",
+        "new/<slug:requester>/<slug:receiver>",
         views.ChatRoomCreateView.as_view(),
         name="chat_room_create",
     ),
