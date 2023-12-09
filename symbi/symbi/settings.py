8d4a13187cc817a693ce567cf57f8715348ba9cf
Utsav Oza <utsavoza@users.noreply.github.com>
2023-12-08 23:26:32 -0500

    Add 404.html (#275)

--- symbi/symbi/settings.py
+++ symbi/symbi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "symbi-env.eba-imnmcswt.us-east-1.elasticbeanstalk.com",
